89377ec6ed109d3bee2897ce44e6e08f7096f0b1
Leo Feyer <github@contao.org>
2013-09-16 13:13:26 +0200

    Replace insert tags in the details view of the listing module (see #6120)

--- system/modules/listing/ModuleListing.php
+++ system/modules/listing/ModuleListing.php
@@ -10,12 +10,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation, either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program. If not, please visit the Free
  * Software Foundation website at <http://www.gnu.org/licenses/>.
@@ -358,6 +358,7 @@ class ModuleListing extends Module
 		$this->Template->referer = 'javascript:history.go(-1)';
 		$this->Template->back = $GLOBALS['TL_LANG']['MSC']['goBack'];
 		$this->list_info = deserialize($this->list_info);
+		$this->list_info_where = $this->replaceInsertTags($this->list_info_where);
 
 		$objRecord = $this->Database->prepare("SELECT " . $this->list_info . " FROM " . $this->list_table . " WHERE " . (strlen($this->list_info_where) ? $this->list_info_where . " AND " : "") . $this->strPk . "=?")
 									->limit(1)
